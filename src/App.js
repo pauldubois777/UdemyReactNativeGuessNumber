f01ea30fad554574726eb2ba773267b1a4bedec5
Paul DuBois <pauldubois777@yahoo.com>
2019-10-15 14:02:10 -0500

    Folder structure.  Header component

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <View style={styles.screen}>
+      <Header title="Guess a Number" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1
+  }
+});
+export default App;
