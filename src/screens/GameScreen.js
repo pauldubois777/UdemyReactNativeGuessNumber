33bc06182af638364289478bfcb5f6f1eae2526a
Paul DuBois <pauldubois777@yahoo.com>
2019-11-26 09:57:19 -0600

    GameScreen layout

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View
+} from 'react-native';
 
 import Card from '../components/Card';
 import NumberOutput from '../components/NumberOutput';
-import Input from '../components/Input';
 import colors from '../constants/colors';
 
 const generateGuessBetween = (min, max) => {
@@ -15,8 +20,66 @@ const generateGuessBetween = (min, max) => {
 
 const GameScreen = props => {
   const [guess, setGuess] = useState(generateGuessBetween(1, 99));
+
+  const guessHandler = guessStatus => {
+    console.log(guessStatus);
+  };
+
+  return (
+    <TouchableWithoutFeedback>
+      <View style={styles.screen}>
+        <Card style={styles.guessCard}>
+          <Text style={styles.guessLabelText}>Computer's Guess</Text>
+          <NumberOutput number={guess} />
+          <View style={styles.buttonsRow}>
+            <View style={styles.buttonView}>
+              <Button
+                title="Lower"
+                onPress={() => guessHandler('lower')}
+                color={colors.cancel}
+              />
+            </View>
+            <View style={styles.buttonView}>
+              <Button
+                title="Higher"
+                onPress={() => guessHandler('higher')}
+                color={colors.cancel}
+              />
+            </View>
+          </View>
+        </Card>
+      </View>
+    </TouchableWithoutFeedback>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  guessCard: {
+    width: 300,
+    maxWidth: '80%',
+    alignItems: 'center',
+    backgroundColor: colors.card,
+    marginTop: 20
+  },
+  guessLabelText: {
+    fontSize: 18
+  },
+  buttonsRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '100%'
+    // paddingHorizontal: 15,
+    // paddingBottom: 15
+  },
+  buttonView: {
+    width: '30%'
+  }
+});
 
 export default GameScreen;
