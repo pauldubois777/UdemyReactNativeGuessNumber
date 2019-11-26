05a106c147ce5e22d68dbd9362018292fe12aa5f
Paul DuBois <pauldubois777@yahoo.com>
2019-11-26 09:34:33 -0600

    Guess generator

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -6,7 +6,16 @@ import NumberOutput from '../components/NumberOutput';
 import Input from '../components/Input';
 import colors from '../constants/colors';
 
-const GameScreen = props => {};
+const generateGuessBetween = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
+};
+
+const GameScreen = props => {
+  const [guess, setGuess] = useState(generateGuessBetween(1, 99));
+};
 
 const styles = StyleSheet.create({});
 
