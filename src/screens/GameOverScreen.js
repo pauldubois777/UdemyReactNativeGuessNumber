b93ace2398d24ef6241d8341026aa99f474b7069
Paul DuBois <pauldubois777@yahoo.com>
2020-02-07 15:57:52 -0600

    Dyanmic sizing for Game Over screen

--- src/screens/GameOverScreen.js
+++ src/screens/GameOverScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Dimensions, Image, ScrollView, StyleSheet, View } from 'react-native';
 
 import Card from '../components/Card';
@@ -6,13 +6,34 @@ import colors from '../constants/colors';
 import TextStyled from '../components/TextStyled';
 import ButtonPrimary from '../components/ButtonPrimary';
 
+const calcImageWidth = windowDimensions => {
+  return windowDimensions.width > 350 ? 300 : 150;
+};
+
 const GameOverScreen = props => {
+  const [imageWidth, setImageWidth] = useState(
+    calcImageWidth(Dimensions.get('window'))
+  );
+  useEffect(() => {
+    const updateLayout = () => {
+      setImageWidth(calcImageWidth(Dimensions.get('window')));
+    };
+    Dimensions.addEventListener('change', updateLayout);
+    return () => Dimensions.removeEventListener('change', updateLayout);
+  }, []);
+
   return (
     <ScrollView>
       <View style={styles.screen}>
         <Card style={styles.gameOverCard}>
           <TextStyled style={styles.titleText}>Game Over</TextStyled>
-          <View style={styles.imageContainer}>
+          <View
+            style={{
+              ...styles.imageContainer,
+              width: imageWidth,
+              height: imageWidth,
+              borderRadius: imageWidth / 2
+            }}>
             <Image
               style={styles.image}
               source={require('../../assets/images/success.png')}
@@ -72,9 +93,6 @@ const styles = StyleSheet.create({
     height: '100%'
   },
   imageContainer: {
-    height: Dimensions.get('window').width > 350 ? 300 : 150,
-    width: Dimensions.get('window').width > 350 ? 300 : 150,
-    borderRadius: Dimensions.get('window').width > 350 ? 150 : 75,
     borderWidth: 3,
     borderColor: 'black',
     overflow: 'hidden',
